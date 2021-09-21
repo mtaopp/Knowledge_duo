0866239c470bd7194e83f9ce4805965e69583ee6
m.ta.codes <m.ta.opportunity@gmail.com>
2021-09-21 09:09:36 +0100

    flexbox

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,8 +9,48 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <h1><a href="/">Knowledge Tree</a></h1>
-    @yield('content')
-    <div><b>Cheese footer</b></div>
+
+    <div class="container1">
+        <div class="wrapper">
+            <div id="left"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas dolorum perferendis illum saepe repudiandae expedita accusantium hic aperiam odit alias, quaerat tempora deleniti deserunt facere quibusdam quis sed facilis ut. Dolor provident modi debitis!</p>
+            <a href="">Logo</a>
+            <footer><b>Cheese footer</b></footer>
+            </div>
+            <div id="right">asdsaddas</div>
+        </div>
+
+    </div>
+
+
+<!--
+<div class="container1">
+    <div class="flex-container1">
+        <div id="left" class="container1">
+            <div class="flex-container1">
+
+                <div id="logo">
+                    <h1><a href="/">Knowledge Tree</a></h1><br>
+                </div>
+
+                <div id="topics">
+                    Topics
+                    </div>
+
+            </div>
+        </div>
+
+        <div id="right">
+            <div class="container1">
+
+                <div class="categories">
+                    @yield('content')
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+-->
+
 </body>
 </html>
